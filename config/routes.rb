4b546f6a1b7dc1e4ba3fcee9e3c1cdfecaed6a1d
kizuku <tukada2947@yahoo.co.jp>
2019-08-05 16:51:47 +0900

    save

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,13 @@ Rails.application.routes.draw do
   
   # public/indexは後々ログイン時のルートに設定する。設定方法は後々調べる。
   get "public/index" => "public#index"
-  
+
   get "users" => "users#index", as: :mypage
-  resources :users, only:[:index, :edit, :show]
-  
+  resources :users, only:[:edit, :show]  
   resources :categories, only: [:index]
   resources :brands, only: [:index]
   resources :trade, only: [:show]
   resources :items do
     resources :comments, only: [:index, :create]
   end
-  
 end
