7a908b448ec72b554b0cc37e75b61654bd66b926
kizuku <tukada2947@yahoo.co.jp>
2019-08-08 12:23:46 +0900

    save

--- db/migrate/20190808024028_create_items.rb
+++ db/migrate/20190808024028_create_items.rb
@@ -13,9 +13,6 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer      :buyer
       t.text         :detail
 
-      t.references   :photo_id, foreign_key: true        
-      t.references   :brand_id, foreign_key: true       
-      t.references   :category_id, foreign_key: true
       t.timestamps
     end
   end
