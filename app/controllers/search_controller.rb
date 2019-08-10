1c00ac2f3a2d7b2e35f37e06e1d5999bfb713b0b
中嶋梓 <usaichigo12000@gmail.com>
2019-08-10 18:31:41 +0900

    ファイルを作りました

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,16 @@
-lass SearchController < ApplicationController
-  def search
-    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order("id DESC").page(params[:page]).per(15)
+class SearchController < ApplicationController
+  def index
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%")
     if params[:keyword] == ""
       redirect_to '/items/search?utf8=✓&keyword=+++'
     end
     if @items.count == 0
       @all_items = Item.limit(25).order("id ASC")
+    end
+
+    def index
+      @q = tasks.ransack(params[:q])
+      @tasks = @q.result(distinct: true).resent
     end
   end
 end
