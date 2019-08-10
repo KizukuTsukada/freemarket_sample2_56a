ff8e48e68b3d7c7cfa11101e79de6644ff126d37
kizuku <tukada2947@yahoo.co.jp>
2019-08-10 18:04:39 +0900

    itemsテーブルカラム名変更

--- db/migrate/20190810085106_rename_titre_column_to_items.rb
+++ db/migrate/20190810085106_rename_titre_column_to_items.rb
@@ -1,6 +1,6 @@
 class RenameTitreColumnToItems < ActiveRecord::Migration[5.2]
   def change
-    rename_column :items, :seller, :seller_id
+    rename_column :items, :seler, :seler_id
     rename_column :items, :buyer,  :buyer_id
   end
 end
