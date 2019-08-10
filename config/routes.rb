da2ab41505719655c8af328c1cfaf9f3386201a8
わたり。 <yue.hinako.0175@gmail.com>
2019-08-10 13:17:00 +0900

    mede signup-controller, set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,44 @@
 Rails.application.routes.draw do
 
+  root "home#index"
+
+
  # deviseのカスタマイズコントローラ用ルーティン。新規登録時にしよう。
-  devise_for :users, 
+  devise_for :users,
   controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
 
-resources :users, only:[:index, :show] do
-  collection do
-    # 新規登録
-    get 'sms_confirmation'
-    get 'delivery_address'
-    get 'pay_way'
-    get 'complete_signup'
-    get 'add_credit'
+ # signup(session)
+  resources :signup, only:[:index] do
+    collection do
+      get 'registration'
+      get 'sms_confirmation'
+      get 'delivery_address'
+      get 'pay_way'  # 入力が全て完了
+      get 'complete_signup'  #登録完了後
+    end
   end
-end
 
-  root "home#index"
-  
-  # public/indexは後々ログイン時のルートに設定する。ログイン機能実装後に実装する。
-  get "public/index", to: "public#index"
 
+ # users
+  resources :users, only:[:index, :show] do
+    # 新規登録
+    collection do
+      # get 'sms_confirmation'
+      # get 'delivery_address'
+      # get 'pay_way'
+      # get 'complete_signup'
+      get 'add_credit'
+    end
+  end
+
+
+ # mypages
   get "mypages", to: "mypages#index", as: :mypage
-
   get "mypages/edit", to: "mypages#edit"
+
   resources :mypages, only:[:index, :show] do
     collection do
       get 'logout'
@@ -34,8 +47,16 @@ end
     end
   end
 
+
+ # public/indexは後々ログイン時のルートに設定する。ログイン機能実装後に実装する。
+  get "public/index", to: "public#index"
+
+
+ # その他のルーティング
   resources :categories, only: [:index]
   resources :brands, only: [:index]
   resources :trade, only: [:index, :show]
   resources :items
+
+
 end
