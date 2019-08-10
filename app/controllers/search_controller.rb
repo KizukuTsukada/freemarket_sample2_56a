bec1042794799cb54828137651d2a42a45f9a602
中嶋梓 <usaichigo12000@gmail.com>
2019-08-10 20:52:08 +0900

    エラー解消

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,13 @@
-def search
-  @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order("id DESC")
-  if params[:keyword] == ""
-    redirect_to '/items/search?utf8=✓&keyword=+++'
-  end
-  if @items.count == 0
-    @all_items = Item.limit(25).order("id ASC")
+class SearchController < ApplicationController
+  def index
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order("id ASC")
+    if params[:keyword] == ""
+      redirect_to '/items/search?utf8=✓&keyword=+++'
+    end
+    if @items.count == 0
+      @all_items = Item.limit(25).order("id ASC")
+    end
   end
 end
+
+  
