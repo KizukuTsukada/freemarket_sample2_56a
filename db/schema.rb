a431307ed301935840315f2113c9ede29e8c142e
kizuku <tukada2947@yahoo.co.jp>
2019-08-10 18:30:12 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_043102) do
+ActiveRecord::Schema.define(version: 2019_08_10_085106) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2019_08_10_043102) do
     t.integer "deliver_way"
     t.integer "deliver_data"
     t.integer "deliver_fee"
-    t.integer "saler"
-    t.integer "buyer"
+    t.integer "saler_id"
+    t.integer "buyer_id"
     t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
