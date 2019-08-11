afb0ffb68721ed112254494a912bf6f57275da1c
kizuku <tukada2947@yahoo.co.jp>
2019-08-11 11:21:57 +0900

    save

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :saler, class_name: "User"
+  belongs_to :buyer, class_name: "User"
+  
   has_many :users, through: :trades
   has_many :photos
   belongs_to :brand
