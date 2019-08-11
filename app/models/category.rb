40dfd8b55c47ff7dfc4f25440a4e1cddc091147f
Kengo Baba <kengo0316.giants@gmail.com>
2019-08-11 16:31:55 +0900

    save

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  has_ancestry
 end
+
+mens = Category.create(name: "メンズ")
+
+mens_tops = mens.children.create(name: "トップス")
+
+mens_tops.children.create([{name: "Tシャツ"}, {name: "ジャケット"}])
