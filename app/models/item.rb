cc2421d8013f308187d91231effdf4f21a0c305e
kizuku <tukada2947@yahoo.co.jp>
2019-08-11 16:54:36 +0900

    save

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   has_many :photos
   belongs_to :brand
   belongs_to :category
+
+  
+  # itemsテーブルのsaler_idとbuyer_idをusersテーブルと紐ずけ
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User"
 end
