211a91788b460a5f6a0e6b529b89a3a0ffde516d
わたり。 <yue.hinako.0175@gmail.com>
2019-08-11 20:42:37 +0900

    fixed no params error

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -8,7 +8,7 @@ class SignupController < ApplicationController
     @user.build_profile
   end
 
-  def sms_confiemation
+  def sms_confirmation
     session[:nickname] = user_params[:nickname]
     session[:email] = user_params[:email]
     session[:password] = user_params[:password]
@@ -25,6 +25,7 @@ class SignupController < ApplicationController
   def pay_way
     @user = User.new
     @user.build_profile(user_params[:profile_attributes]) 
+    @user.build_credit
   end
 
 
@@ -33,7 +34,7 @@ class SignupController < ApplicationController
       nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
       email: session[:email],
       password: session[:password],
-      password_confirmation: session[:password_confirmation],
+      password_confirmation: session[:password_confirmation]
     )
     @user.build_profile(user_params[:profile_attributes]) # 入力値を引数で渡す
     @user.build_credit(user_params[:credit_attributes]) # 入力値を引数で渡す
@@ -59,7 +60,7 @@ class SignupController < ApplicationController
       :password, 
       :password_confirmation, 
       profile_attributes: [:id, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :postal_code, :prefectures, :city, :address1, :address2, :phone_number],
-      credirt_attreibutes: [:id, :card_no, :validity_year, :validity_month, :security_no]
+      credit_attreibutes: [:id, :card_no, :validity_year, :validity_month, :security_no]
     )
   end
 
