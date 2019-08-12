05fbb744d747d3fdb0fb619e01f7d3be2c1ab2b8
わたり。 <yue.hinako.0175@gmail.com>
2019-08-12 12:39:01 +0900

    add each user_params[:profile_attributes] into session, and merge! each session (profile)

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -9,34 +9,32 @@ class SignupController < ApplicationController
   end
 
   def sms_confirmation
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:password_confirmation] = user_params[:password_confirmation]
+
+    session[:user_params] = user_params
+    session[:profile_attributes1] = user_params[:profile_attributes]
     @user = User.new
-    @user.build_profile(user_params[:profile_attributes]) 
+    @user.build_profile
   end
 
   def delivery_address
+    session[:profile_attributes2] = user_params[:profile_attributes]
+    session[:profile_attributes2].merge!(session[:profile_attributes1])
     @user = User.new
-    @user.build_profile(user_params[:profile_attributes]) 
+    @user.build_profile
   end
 
   def pay_way
+    session[:profile_attributes3] = user_params[:profile_attributes]
+    session[:profile_attributes3].merge!(session[:profile_attributes2])
     @user = User.new
-    @user.build_profile(user_params[:profile_attributes]) 
+    @user.build_profile
     @user.build_credit
   end
 
 
   def create
-    @user = User.new(
-      nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
-      email: session[:email],
-      password: session[:password],
-      password_confirmation: session[:password_confirmation]
-    )
-    @user.build_profile(user_params[:profile_attributes]) # 入力値を引数で渡す
+    @user = User.new(session[:user_params])
+    @user.build_profile(session[:profile_attributes3] ) # 入力値を引数で渡す
     @user.build_credit(user_params[:credit_attributes]) # 入力値を引数で渡す
     if @user.save
       session[:id] = @user.id
@@ -60,7 +58,7 @@ class SignupController < ApplicationController
       :password, 
       :password_confirmation, 
       profile_attributes: [:id, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :postal_code, :prefectures, :city, :address1, :address2, :phone_number],
-      credit_attreibutes: [:id, :card_no, :validity_year, :validity_month, :security_no]
+      credit_attributes: [:id, :card_no, :validity_year, :validity_month, :security_no]
     )
   end
 
