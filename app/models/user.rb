c9ac11e0cb17b30689d1433ce23857c36968fd83
simon-group <saku_crownheart@yahoo.co.jp>
2019-08-12 14:37:38 +0900

    コミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,30 @@ class User < ApplicationRecord
   # buyer_idがNULLのとき →:saling_items, -> { where("buyer_id is NULL") }(SQL文)
   has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Item"
+
+  def self.find_oauth(auth)
+    # snsのユーザーのidとproviderを変数に代入
+    uid = auth.uid
+    provider = auth.provider
+    # DBにすでにsns情報を保存しているユーザーのuidとproviderを代入
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    # DBにすでに存在していればそのユーザーを探してそのユーザーを返す
+    if snscredential.present?
+      user = User.where(id: snscredential.user_id).first
+    else
+    # snsでのアプリへの登録はないがsns上のemailと同一のemailアドレスを持つユーザーの場合はsnsの情報をsnsCredentialsテーブルに追加
+      user = User.where(email: auth.info.email).first
+      if user.present?
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      else
+    # snsでの登録もユーザーの登録もない場合はnilを返す
+        user = nil
+      end
+    end
+    return user
+  end
 end
