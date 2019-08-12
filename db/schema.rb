39787f00ce37555286ff3ae2139574effcbf9b16
simon-group <saku_crownheart@yahoo.co.jp>
2019-08-12 13:38:12 +0900

    コミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_08_12_034636) do
+ActiveRecord::Schema.define(version: 2019_08_12_040259) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
