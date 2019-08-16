54ecff8c5a704e04eb793d2c029a36665baf252a
simon-group <saku_crownheart@yahoo.co.jp>
2019-08-17 00:47:12 +0900

    syuseicommit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,7 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+#credencial.ymlを読み込む変数に変更
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
