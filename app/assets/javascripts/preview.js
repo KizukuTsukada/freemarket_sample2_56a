bd3129105dbd8566657103a53a8b1048bdc7e8f4
simon-group <saku_crownheart@yahoo.co.jp>
2019-08-21 16:35:24 +0900

    save

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,23 +1,18 @@
-$(function(){
-  $('#preview').change(function(e){
-    //ファイルオブジェクトを取得する
-    var file = e.target.files[0];
-    var reader = new FileReader();
+$( document ).on('turbolinks:load', function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
  
-    //画像でない場合は処理終了
-    if(file.type.indexOf("image") < 0){
-      alert("画像ファイルを指定してください。");
-      return false;
+      reader.onload = function (e) {
+        $('#avatar_img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
     }
+  }
  
-    //アップロードした画像を設定する
-    reader.onload = (function(file){
-      return function(e){
-        $("#img1").attr("src", e.target.result);
-        $("#img1").attr("title", file.name);
-      };
-    })(file);
-    reader.readAsDataURL(file);
- 
+  $("#post_img").change(function(){
+    $('#avatar_img_prev').removeClass('hidden');
+    $('.avatar_present_img').remove();
+    readURL(this);
   });
 });
