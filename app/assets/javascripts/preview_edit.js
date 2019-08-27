c4c0e1340f43946f95f3c0b5eb3f917fca08b429
simon-group <saku_crownheart@yahoo.co.jp>
2019-08-28 00:21:15 +0900

    save

--- app/assets/javascripts/preview_edit.js
+++ app/assets/javascripts/preview_edit.js
@@ -7,22 +7,21 @@ $(document).on('turbolinks:load', function() {
       reader = new FileReader(),
       $preview = $("#preview");
 
-      var img = '<div class="img_view"><img alt="" class="img"><p><a href="#" class="img_del">削除する</a></p></div>';
       reader.onload = (function(file) {
         return function(e) {
           $preview.empty();
           $preview.append($('<img>').attr({
             src: e.target.result,
             width: "100%",
-            height: "100%",
-            class: "preview",
+            height: "162px",
+            class: "preview-image",
             title: file.name
           }));
         };
       })(file);
       reader.readAsDataURL(file);
-      $(document).on("click", "#preview", function () {
-        $(this).parent().remove();
-      });
+    });
   });
-});
+    $(".remove_btn").on("click", function() {
+      $(this).parent().remove();
+    });
